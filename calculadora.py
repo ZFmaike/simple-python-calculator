e1e23bcbff4f4133bd016d973520c9d9ce0685b5
ZFmaike <168730291+ZFmaike@users.noreply.github.com>
2024-05-03 07:36:50 -0300

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,8 @@
+def soma (a, b)
+    return a + b
+
+if _name_=="_main_":
+    num1 = float(input("digite o primeiro número: "))
+    num2 = float(input("digte o segundo número: "))
+    resultado = soma(num1, num2)
+    print(f"resultado: {resultado}")
